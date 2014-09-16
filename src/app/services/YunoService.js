d94898a25fcfd39d34d8deeac7a9aacddb381c78
vzijden <vincentvanderzijden@192.168.1.105>
2014-09-16 17:08:07 +0200

    Revert "naamvraag, nieuw algoritme, backbutton"
    This reverts commit ff8c86581b22cd320ad98dbb9929a74fedced472.

--- src/app/services/YunoService.js
+++ src/app/services/YunoService.js
@@ -1,20 +1,6 @@
-angular.module('yuno').service("yunoService", function () {
-
-    this.price = 40;
-    this.name = ""
-    this.filter = {
-        "Man" : 0,
-        "beleven": 0,
-        "luxe": 0,
-        "praktisch": 0,
-        "uniek": 0,
-        "sportief": 0,
-        "culinair": 0,
-        "relaxt": 0,
-        "binnen": 0,
-        "buiten": 0,
-        "beiden":0 //toegevoegd ipv beiden als buiten en binnen
-        };
-
+angular.module('yuno').service("yunoService", function(){
+	
+	this.price = 40;
+	this.filter = {"beleven":0,"luxe":0,"praktisch":1,"uniek":0,"sportief":0,"culinair":1,"relaxt":0,"binnen":0,"buiten":1};
 
 });
