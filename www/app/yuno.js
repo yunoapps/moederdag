32c9d168614da0ba8c65634d1960e6bcab7b2e93
vzijden <vincentvanderzijden@192.168.1.105>
2014-09-16 17:06:58 +0200

    Revert "grunt vergeten"
    This reverts commit e8268c17a91bcd93be5730c4e2b0bded2d80ee4a.

--- www/app/yuno.js
+++ www/app/yuno.js
@@ -28,85 +28,76 @@ angular.module('yuno', [
      
       // configure html5 to get links working on jsfiddle
       //$locationProvider.html5Mode(true);
-  });angular.module('yuno').controller("GiftsController", function ($log, $scope, $location, $http, $filter, yunoService) {
+  });angular.module('yuno').controller("GiftsController", function($log, $scope, $location, $http, $filter, yunoService) {
 
-    var step = 0;
-    var gifts = [];
+	var step = 0;
+	var gifts = [];
 
-    $scope.price = yunoService.price;
-    $scope.filter = yunoService.filter;
+	$scope.price = yunoService.price;
+	$scope.filter = yunoService.filter;
+	
+	$http.get("data/gifts.json").success(function(data){
+		gifts = data;
+		gifts = $filter('filter')(gifts, $scope.giftFilter);
+		gifts = $filter('orderBy')(gifts, "-waardering");
+	})
 
-    $http.get("data/gifts.json").success(function (data) {
-        gifts = data;
-//		gifts = $filter('filter')(gifts, $scope.giftFilter);
-        gifts = $filter('orderBy')(gifts, $scope.calculateMatchFactor, true);
+	if(!$scope.filter){
+		//$location.path("/price");
+	}
 
-    })
+	$scope.gift = function(){
+		$log.debug(gifts);
+		return gifts[step];
+	}
 
-    if (!$scope.filter) {
-        //$location.path("/price");
-    }
+	$scope.giftFilter = function(gift){
+		$log.debug("Filter", gift);
 
-    $scope.gift = function () {
-        $log.debug(gifts);
-        return gifts[step];
-    }
+		gift.image = "products/" + gift.refname + ".jpg";
 
+		if($scope.filter){
+			return (
+				gift.beleven == $scope.filter.beleven &&
+				(
+					gift.luxe == $scope.filter.luxe ||
+					gift.praktisch == $scope.filter.praktisch ||
+					gift.uniek == $scope.filter.uniek
+				) && (
+					gift.sporten == $scope.filter.sporten ||
+					gift.eten == $scope.filter.eten 
+				) && (
+					gift.binnen == $scope.filter.binnen ||
+					gift.buiten == $scope.filter.buiten 
+				) &&
+				(
+					gift.price < $scope.price ||
+					$scope.price == 100
+				)
+			);
+		}
+	}
 
-    $scope.calculateMatchFactor = function (gift) {
-        gift.image = "products/" + gift.refname + ".jpg";
-        var filter = $scope.filter;
-        var punten = gift.man * filter.Man +
-            gift.vrouw * (-1 * filter.Man) +
-            gift.beleven * filter.beleven +
-            gift.luxe * filter.luxe +
-            gift.praktisch * filter.praktisch +
-            gift.uniek * filter.uniek +
-            gift.sportief * filter.sportief +
-            gift.culinair * filter.culinair +
-            gift.gezellig * filter.relaxt +
-            gift.binnen * filter.binnen +
-            gift.buiten * filter.buiten +
-            gift.beiden * filter.beiden;
+	$scope.next = function(){
+		if(step < gifts.length-1){
+			step++;
+		}
+	}
 
-        var prijsafwijking = (1 - (gift.price - $scope.price) / gift.price);
-        var prijspunten;
-        if (prijsafwijking < 1)
-            prijspunten = prijsafwijking * 45;
-        else
-            prijspunten = 45 / prijsafwijking;
+	$scope.back = function(){
+		if(step > 0){
+			step--
+		}
+	}	
 
-        var matchfacto = prijspunten + punten;
-        gift.matchfactoRound = Math.round(matchfacto);
+	$scope.order = function(){
+		var gift = gifts[step];
+		var url = "http://partnerprogramma.bol.com/click/click?p=1&s=27614&t=p&sec=all:&f=PDL&";
+		url += "pid=" + gift.refid;
+		url += "&name=YUNO&subid=Moederdag";
+		var ref = window.open(url, '_system', 'location=yes');
 
-        return matchfacto;
-    }
-
-
-    $scope.next = function () {
-        if (step < gifts.length - 1) {
-            step++;
-        }
-    }
-
-    $scope.back = function () {
-        if (step > 0) {
-            step--
-        }
-    }
-
-    $scope.first = function () {
-        step = 0;
-    }
-
-    $scope.order = function () {
-        var gift = gifts[step];
-        var url = "http://partnerprogramma.bol.com/click/click?p=1&s=27614&t=p&sec=all:&f=PDL&";
-        url += "pid=" + gift.refid;
-        url += "&name=YUNO&subid=Moederdag";
-        var ref = window.open(url, '_system', 'location=yes');
-
-    }
+	}	
 
 });angular.module('yuno').controller("PriceController", function($log, $scope, $location, yunoService) {
 
@@ -136,64 +127,40 @@ angular.module('yuno', [
 		$location.path("/questions");
 	}
 	
-});angular.module('yuno').controller("QuestionsController", function ($log, $scope, $location, $http, yunoService) {
+});angular.module('yuno').controller("QuestionsController", function($log, $scope, $location, $http, yunoService) {
 
-    $scope.step = 0;
-    $scope.multipleChoiceDisplay = true;
-    $scope.filter = {};
-    $scope.questions = [];
+	$scope.step = 0;
+	$scope.filter = {};
+	$scope.questions = [];
 
-    $http.get("data/questions.json").success(function (data) {
-        $scope.questions = data;
-    });
+	$http.get("data/questions.json").success(function(data){
+		$scope.questions = data;
+	});
 
-    $scope.setName = function () {
-        next();
-        yunoService.name = $scope.name;
+	$scope.next = function(answer){
+		$log.debug("Answer", answer);
+		var options = $scope.questions[$scope.step]['filter'][answer];
+		$log.debug("Options", options);
 
-        angular.forEach($scope.questions, function(value, key){
-            $scope.questions[key].question = value.question.replace("[NAAM]", $scope.name);
+		angular.forEach(options, function(value, key){
+			$scope.filter[key] = value;
+		});
 
-        });
+		if(($scope.step + 1) < $scope.questions.length){
+			$scope.step++;
+		}else{
+			yunoService.filter = $scope.filter;
+			$location.path("/gifts");
+		}
+	};
 
-    };
-
-
-    $scope.next = function (answer) {
-        $log.debug("Answer", answer);
-        var options = $scope.questions[$scope.step]['filter'][answer];
-        $log.debug("Options", options);
-
-        angular.forEach(options, function (value, key) {
-            $scope.filter[key] = value;
-        });
-
-        next();
-
-    };
-
-    function next() {
-        if ($scope.step == 0){
-            $scope.multipleChoiceDisplay = false;
-        } else {
-            $scope.multipleChoiceDisplay = true;
-        }
-
-        if (($scope.step + 1) < $scope.questions.length) {
-            $scope.step++;
-        } else {
-            yunoService.filter = $scope.filter;
-            $location.path("/gifts");
-        }
-    }
-
-    $scope.back = function (answer) {
-        if ($scope.step > 0) {
-            $scope.step--;
-        } else {
-            $location.path("/price");
-        }
-    };
+	$scope.back = function(answer){
+		if($scope.step > 0){
+			$scope.step--;
+		}else{
+			$location.path("/price");
+		}
+	};
 
 });angular.module('yuno').controller("StartController", function($log, $scope, $location) {
 
@@ -201,23 +168,9 @@ angular.module('yuno', [
 		$location.path("/price");
 	}
 	
-});angular.module('yuno').service("yunoService", function () {
-
-    this.price = 40;
-    this.name = ""
-    this.filter = {
-        "Man" : 0,
-        "beleven": 0,
-        "luxe": 0,
-        "praktisch": 0,
-        "uniek": 0,
-        "sportief": 0,
-        "culinair": 0,
-        "relaxt": 0,
-        "binnen": 0,
-        "buiten": 0,
-        "beiden":0 //toegevoegd ipv beiden als buiten en binnen
-        };
-
+});angular.module('yuno').service("yunoService", function(){
+	
+	this.price = 40;
+	this.filter = {"beleven":0,"luxe":0,"praktisch":1,"uniek":0,"sportief":0,"culinair":1,"relaxt":0,"binnen":0,"buiten":1};
 
 });
